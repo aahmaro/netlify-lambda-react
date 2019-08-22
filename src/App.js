b8a456e33c9cbbdd2145d4076595be752e2a8c72
aahmaro <ahmaro.ahmad@gmail.com>
2019-08-22 10:44:42 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState();
+  const handleFetchUsers = () => {
+    const fetchUsers = async () =>
+      await (await fetch("/.netlify/functions/getusers")).json();
+
+    fetchUsers().then(data => {
+      setUsers(data);
+    });
+  };
+
+  useEffect(() => {
+    handleFetchUsers();
+  });
+
+  console.log(users);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map(item => (
+        <p>{item}</p>
+      ))}
     </div>
   );
 }
